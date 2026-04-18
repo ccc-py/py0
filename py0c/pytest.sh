62e797149e7938085c43335c59a1a067331f0f11
ccckmit <ccckmit@gmail.com>
2026-04-18 16:08:50 +0800

    split py0i, py0c

---
+++ py0c/pytest.sh
@@ -0,0 +1,11 @@
+set -x
+
+python py0c.py py/hello.py -o qd/hello.qd
+python py0c.py py/fact.py -o qd/fact.qd
+python py0c.py py/json.py -o qd/json.qd
+
+python qd0vm.py qd/hello.qd
+python qd0vm.py qd/fact.qd
+python qd0vm.py qd/json.qd
+
+
--- qd0vm_test.sh
+++
@@ -1,4 +0,0 @@
-set -x
-python qd0vm.py qd/fact.qd
-python qd0vm.py qd/hello.qd
-
